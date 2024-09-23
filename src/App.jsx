fdef97baf3798b293383e60ee719699f5596e2fd
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-09-23 14:38:54 +0200

    Added heading, array with initial values, listing items from array

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,29 @@
 export default function App(){
-  return "Hello World"
+  const snus = [
+    {
+      name: 'Kapten',
+      grade: 5,
+    }, 
+    {
+      name: 'Klaven',
+      grade: 4,
+    },
+  ]
+  
+  
+  return (
+    <>
+    <h1>Snus graded</h1>
+    <br/>
+    <ul>
+      {snus.map(snus => {
+        return (
+          <li key={snus.name}>
+            <span>{snus.name} {snus.grade}</span>
+          </li>
+        )
+      })}
+    </ul>
+    </>
+  )
 }
