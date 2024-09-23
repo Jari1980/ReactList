5da7981daa076b1ea66e4a780911e4d3d7ca9c62
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-09-23 15:52:30 +0200

    Cleared some errors, added new array, figuring a way to populate this with values from first, or doing it in a other way

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,72 @@
-import 'bootstrap/dist/css/bootstrap.css';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
-export default function App(){
+export default function App() {
+  const [name, setName] = useState('')
+  const [grade, setGrade] = useState('')
+  const [snuss, setSnuss] = useState('')
   const snus = [
     {
-      name: 'Kapten',
+      name: "Kapten",
       grade: 5,
-    }, 
+    },
     {
-      name: 'Klaven',
+      name: "Klaven",
       grade: 4,
     },
-  ]
-  
-  
+  ];
+
   return (
     <>
-    <h1>Snus graded</h1>
-    <br/>
-    <ul className="list-group">
-      {snus.map(snus => {
-        return (
-          <li key={snus.name} className="list-group-item">
-              <span>Snus: {snus.name}   Grade: {snus.grade}</span>
-          </li>
-        )
-      })}
-    </ul>
-    <br/>
-    <br/>
-    <form>
-      <h2>Add Snus to the list</h2>
-  <div className="form-group">
-    <label for="snusInput">Enter Snus sort</label>
-    <input type="text" class="form-control" id="snusInput" placeholder="Enter new snus"/>
-  </div>
-  <div class="form-group">
-  <label for="snusGrade">Enter Snus grade</label>
-  <input type="number" class="form-control" id="snusGrade" placeholder="Enter Snus grade 1-5"/>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+      <h1>Snus graded</h1>
+      <br />
+      <ul className="list-group">
+        {snus.map((snus) => {
+          return (
+            <li key={snus.name} className="list-group-item">
+              <span>
+                Snus: {snus.name} Grade: {snus.grade}
+              </span>
+            </li>
+          );
+        })}
+      </ul>
+      <br />
+      <br />
+      <form>
+        <h2>Add Snus to the list</h2>
+        <div className="form-group">
+          <label>Enter Snus sort</label>
+          <input
+            type="text"
+            className="form-control"
+            id="snusInput"
+            placeholder="Enter new snus"
+            value={name}
+            onChange={e => setName(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label>Enter Snus grade</label>
+          <input
+            type="number"
+            className="form-control"
+            id="snusGrade"
+            placeholder="Enter Snus grade 1-5"
+            value={grade}
+            onChange={e => setGrade(e.target.value)}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={() => {
+          setSnuss([
+            ...snuss,
+            {name: name, grade: grade},
+            console.log("clicked added " + name + " " + grade)
+          ])
+        }}>
+          Submit
+        </button>
+      </form>
     </>
-  )
+  );
 }
