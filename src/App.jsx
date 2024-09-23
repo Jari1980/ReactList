18e70a5122cdd2f5ad813c1e95ff8b0557888b2f
Jari Tenhunen <jari.tenhunen@outlook.com>
2024-09-23 15:09:26 +0200

    Added bootstrap and non functional input form

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.css';
+
 export default function App(){
   const snus = [
     {
@@ -15,15 +17,29 @@ export default function App(){
     <>
     <h1>Snus graded</h1>
     <br/>
-    <ul>
+    <ul className="list-group">
       {snus.map(snus => {
         return (
-          <li key={snus.name}>
-            <span>{snus.name} {snus.grade}</span>
+          <li key={snus.name} className="list-group-item">
+              <span>Snus: {snus.name}   Grade: {snus.grade}</span>
           </li>
         )
       })}
     </ul>
+    <br/>
+    <br/>
+    <form>
+      <h2>Add Snus to the list</h2>
+  <div className="form-group">
+    <label for="snusInput">Enter Snus sort</label>
+    <input type="text" class="form-control" id="snusInput" placeholder="Enter new snus"/>
+  </div>
+  <div class="form-group">
+  <label for="snusGrade">Enter Snus grade</label>
+  <input type="number" class="form-control" id="snusGrade" placeholder="Enter Snus grade 1-5"/>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
     </>
   )
 }
